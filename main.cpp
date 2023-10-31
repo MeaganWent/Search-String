7fa64fb8a696a65e7fb1ca5c9981c47c800cdfa8
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2023-10-31 11:29:50 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
-//Test File
+**String Search II**
+^^^
+
+**_Implement algorithms_**
+  - knuth-morris-pratt
+  - boyer-moore
+
+**_Benchmark all algorithms recording their runtime_**
+
+- generate plots of running time of multiple sizes
