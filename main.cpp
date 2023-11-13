ae23e94a50b61e2106fffce55bd2662dee5cf214
mandrews12 <113190709+mandrews12@users.noreply.github.com>
2023-11-13 08:39:02 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 **String Search II**
 ^^^
 
@@ -8,3 +9,27 @@
 **_Benchmark all algorithms recording their runtime_**
 
 - generate plots of running time of multiple sizes
+*/
+#include "string_search.h"
+#include <iostream>
+#include <string>
+
+int main(int argc,char*argv[]){
+  // get file names from user
+  std::string text_name(argv[1];
+  std::string pattern_name(argv[2];
+
+  //function to get text and pattern from the file
+
+  //function to run string search
+
+  // function to run Boyer Moore string search
+
+  // function to run KMP string search
+
+  // function to output running times
+
+  // function to output if the pattern was found
+
+
+}
