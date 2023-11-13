31f9c9230096243f9347d6079b0e3ccba244eecc
mandrews12 <113190709+mandrews12@users.noreply.github.com>
2023-11-13 08:44:20 -0500

    Update string_search.h

--- string_search.h
+++ string_search.h
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <vector>
+
+Class String_Search {
+  private:
+    std::string text;
+    std::string pattern;
+    std::vector prefix_val;
+
+  public:
+    //function to get text and pattern from the file
+
+    //function to run string search
+
+    // function to run Boyer Moore string search
+
+    // function to run KMP string search
+    std::vector KMP();
+
+    //helper function for kmp
+    void prefix();
+
+    // function to output running times
+
+    // function to output if the pattern was found
+};
+
 
