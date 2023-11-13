3b62c5cf2271ab670ed85161912418e04e57ee87
mandrews12 <113190709+mandrews12@users.noreply.github.com>
2023-11-13 08:47:54 -0500

    Update strin_search.cpp

--- strin_search.cpp
+++ strin_search.cpp
@@ -1,1 +1,29 @@
+#include "string_search.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
 
+
+//function to get text and pattern from the file
+
+//function to run string search
+
+// function to run Boyer Moore string search
+
+// function to run KMP string search
+std::vector String_Search::KMP() {
+  // vector to store an index of the pattern in the text if found
+  std::vector index;
+  int shift = 0;
+  
+  return index;
+};
+
+//helper function for kmp
+void prefix();
+
+// function to output running times
+
+// function to output if the pattern was found
