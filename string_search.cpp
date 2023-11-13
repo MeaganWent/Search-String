840cf0a97a382bb01f811db60ad0321ef550169e
mandrews12 <113190709+mandrews12@users.noreply.github.com>
2023-11-13 08:59:52 -0500

    Update string_search.cpp

--- string_search.cpp
+++ string_search.cpp
@@ -17,6 +17,25 @@ std::vector String_Search::KMP() {
   // vector to store an index of the pattern in the text if found
   std::vector index;
   int shift = 0;
+  int j = 0;
+  char last_char;
+
+  while(shift < text.length() - pattern.length()) {
+    j = shift;
+    bool found = false;
+    while(text[j] == pattern[j] && j < pattern.length()) {
+      j++;
+      if(j = pattern.length()){
+        found = true;
+      }
+      
+    }
+    if(found){
+      vector.push_back(shift);
+    } else {
+      shift = shift + 1 + prefix[last_char];
+    }
+  }
   
   return index;
 };
