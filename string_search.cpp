e043f27de5effe41e599d3bd43733f4bf0f1ce45
mandrews12 <113190709+mandrews12@users.noreply.github.com>
2023-11-14 10:00:42 -0500

    Update string_search.cpp

--- string_search.cpp
+++ string_search.cpp
@@ -19,44 +19,52 @@ std::vector<int> String_Search::KMP() {
   int shift = 0;
   int j = 0;
   char last_char;
+  
   // goes through entire text but not always by a shift of one
   while(shift < text.length() - pattern.length()) {
     j = shift;
     bool found = false;
+    
     // checks each character in the pattern to make sure all match
     while(text[j] == pattern[j] && j < pattern.length()) {
       last_char = pattern[j];
-      // end of pattern is reached so match was found
+      // end of the pattern is reached so the match was found
       if(j = pattern.length()){
         found = true;
       }
       j++;
     }
+    // determines if a shift should be called from lookup or if a match was found
     if(found){
       index.push_back(shift);
+      shift += 1;
     } else {
       shift = shift + 1 + prefix_val[last_char];
     }
   }
-  
+
+  // returns all the index(es) where a match was found
   return index;
 }
 
 //helper function for kmp to create a vector for looking up num of shifts based on the character
 void String_Search::prefix() {
+  
   // initialize vector of length pattern to 0's
   for(int i = 0; i < pattern.length(); i++) {
     prefix_val.push_back(0);
   }
 
-  // goes through pattern to find number of shifts possible
+  // goes through the pattern to find the number of shifts possible to skip
   int length = 0;
   for(int j = 0; j < pattern.length();j++){
+    
     // a shift is possible
     if(pattern[length] == pattern[j]) {
       length += 1;
       prefix_val[j] = length;
     }
+    
     // a shift is not possible
     if(pattern[length] != pattern[j] && length != 0) {
       length = prefix_val[length - 1];
