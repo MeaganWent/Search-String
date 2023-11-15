6b5839408b9d60594f85f69e7a5a37817eda9e34
Meagan Went <106477587+MeaganWent@users.noreply.github.com>
2023-11-15 09:51:03 -0500

    Create BoyerMoore.cpp

---
+++ BoyerMoore.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+This algorithm follows the Boyer-Moore search string which aims to be an effiencent string search. 
+This algorithm searches from the start of the looking for string, then will find where that matches up in the string. From there it will take the first misplaced
+values and search for its correctly value until it finds the correct values for all given integers.  
+*/
+
+!!! NOT COMPLETED !!!
+// input - takes in Correct which is the string we're looking for, and Search which is the string we're searching through
+// implement badCharHeuristic 
+  // all occurrences as -1
+  // Fill the actual value of last occurrence of a character
