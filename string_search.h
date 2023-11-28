3181497ac44047c295923bb1c6ab49ea327ffa4e
mandrews12 <113190709+mandrews12@users.noreply.github.com>
2023-11-28 11:13:55 -0500

    Update string_search.h

--- string_search.h
+++ string_search.h
@@ -1,39 +1,43 @@
+// .h file to create Class and store methods for string searching
+
 #include <iostream>
 #include <ostream>
 #include <string>
 #include <vector>
 
 class String_Search {
+  // private methods
   private:
+    // vars to store text and pattern to search for
     std::string text = "";
     std::string pattern = "";
+  
     std::vector<int> prefix_val;
     std::vector<int> KMP_index;
     int BM_index;
 
+  // public methods
   public:
     //function to get text and pattern from the file
     String_Search(std::string text, std::string pattern);
 
+    // preprocessing methods
+    void badCharHeuristic(std::string str, int size, int badchar[256]);
+    void prefix();
+
     //function to run string search
     // std::vector<int> Basic();
     // function to run Boyer Moore string search
-    void badCharHeuristic(std::string str, int size, int badchar[256]);
     void BM();
     // function to run KMP string search
     void KMP();
+    
+    // function to record running times
+    std::vector<float> timeFunction();
 
-    //helper function for kmp
-    void prefix();
-
-    // function to output running times
-     std::vector<float> timeFunction();
-
-    // function for outputing results of KMP
+    // function for outputting results
     void KMP_results(float time);
     void BM_results(float time);
-
-    // function to output if the pattern was found
 };
 
 
